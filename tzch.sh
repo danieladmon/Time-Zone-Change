b4767acec62258872a14f8996365f036768137a8
danieladmon <54929117+danieladmon@users.noreply.github.com>
2020-01-07 01:06:00 +0200

    Create tzch.sh

---
+++ tzch.sh
@@ -0,0 +1,15 @@
+###	tzch.sh - v0.1
+### A script for changing the timezone.
+### 
+###	Last update: 17/12/2019
+
+#!/bin/bash
+ARGS=${@}
+TIMEZONE=$(timedatectl list-timezones | grep -m 1 ${ARGS// /_})
+
+if [[ "$TIMEZONE" ]]; then
+        $(sudo timedatectl set-timezone $TIMEZONE)
+        echo "Time zone has change to: $TIMEZONE"
+else
+        echo "Time zone not found"
+fi
